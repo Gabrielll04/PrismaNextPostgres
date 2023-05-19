4a42e491082965365888f438a13cbbacd77f64c3
Gabrielll04 <gabrielggcarneiro04@gmail.com>
2023-05-19 11:36:33 -0300

    Removing line breaks from start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-while ! nc -z postgres 5432; do sleep 1; done
-
-npx prisma db push
-npx prisma generate
-
-npm run dev
+while ! nc -z postgres 5432; do sleep 1; done; npx prisma db push; npx prisma generate; npm run dev
