ea2a9f671c43af22ce0aecedf9b6a9bb4c6667c0
@Gabrielll04 <gabrielggcarneiro04@gmail.com>
2023-05-21 21:38:23 -0300

    Styling post cards

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,7 +6,6 @@ import { redirect } from 'next/navigation'
 
 export default async function HomePage() {
     const user = await getCurrentUser()
-    console.log(user?.image)
     if (!user) {
         redirect('/login')
     }
@@ -14,8 +13,8 @@ export default async function HomePage() {
     const posts = await HomePage.getServerProps()
 
     return (
-        <main className='flex w-screen h-screen'>
-            <article className='mx-auto flex flex-col w-[45rem] h-full'>
+        <main className='flex w-screen h-screen max-h-none overflow-x-hidden overflow-scroll '>
+            <article className='mx-auto flex flex-col w-[45rem] h-full gap-5'>
                 <div className='flex w-full h-20 items-center px-5 mt-5'>
                     <div className='flex items-center space-x-3'>
                         <img src={`${user.image}`} className='w-16 h-16 rounded-full' />
@@ -25,7 +24,7 @@ export default async function HomePage() {
                         <Link href='/posts/create' className='flex items-center justify-center w-20 h-8 rounded text-black font-bold bg-white active:opacity-70'>+ post</Link>
                     </div>
                 </div>
-                <section className='flex flex-col w-full max-h-none'>
+                <section className='flex flex-col w-full h-auto max-h-none space-y-4'>
                     {posts.map((post) => (
                         <PostCard key={post.id} PostContent={post}/>
                     ))}
@@ -36,14 +35,14 @@ export default async function HomePage() {
 }
 
 HomePage.getServerProps = async () => {
-    const posts = await prisma.post.findMany({
+    const allPosts = await prisma.post.findMany({
         where: { published: true },
         include: {
             author: {
-                select: { name: true }
+                select: { name: true, image: true }
             }
         }
     })
 
-    return posts
+    return allPosts
 }
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,8 +1,30 @@
 'use client'
-export default function PostCard({PostContent}) {
+
+import Link from "next/link"
+
+export default function PostCard({PostContent}: any) {
+  console.log(PostContent)
   return (
-    <main>
-        {PostContent.title}
-    </main>
+    <Link href={`/posts/${PostContent.id}`} className='flex w-full min-h-20 h-auto max-h-44 ring-1 ring-neutral-700 rounded p-2'>
+        <section className='flex flex-col w-full h-full space-y-4'>
+          <div className='flex items-center space-x-2'>
+            <img 
+              src={PostContent.author.image} 
+              alt="posts's author image`"
+              className='w-10 h-10 rounded-full'
+            />
+            <span className='font-semibold'>
+              {PostContent.author.name}
+            </span>
+          </div>
+
+          <div className='flex flex-col w-full h-full overflow-clip space-y-2'>
+            <p className='text-2xl font-semibold'>{PostContent.title}</p>
+            <span className='block text-ellipsis overflow-hidden'>
+              {PostContent.content}
+            </span>
+          </div>
+        </section>
+    </Link>
   )
 }
