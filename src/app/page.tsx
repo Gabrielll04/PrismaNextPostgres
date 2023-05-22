8a8b915bbdb8c06eb3f3e38dd2dc4d68b96e21ee
@Gabrielll04 <gabrielggcarneiro04@gmail.com>
2023-05-21 21:40:32 -0300

    Creating and styling each post's dynamic route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { getCurrentUser } from '@/lib/session'
 import LoginButton from '../components/LoginButton'
 
 export default async function Page() {
-  const feed = await Page.getInitialProps()
   const user = await getCurrentUser()
 
   if (user) {
@@ -20,22 +19,6 @@ export default async function Page() {
       <div className='flex justify-center items-center w-full h-32'>
         <LoginButton />
       </div>
-     
     </div>
   )
-}
-
-Page.getInitialProps = async () => {
-  const feed = await prisma.post.findMany({
-    where: { published: true },
-    include: {
-      author: {
-        select: { name: true },
-      },
-    },
-  })
-
-  return {
-    feed
-  }
 }
