af0db5ca9bebd004dd057d9f39346bc987ccb750
@Gabrielll04 <gabrielggcarneiro04@gmail.com>
2023-05-21 21:35:19 -0300

    Changing name of NewPostButton component to BackButton

--- src/app/posts/create/page.tsx
+++ src/app/posts/create/page.tsx
@@ -1,11 +1,11 @@
 'use client'
-import NewPostButton from '@/components/NewPostButton'
+import BackButton from '@/components/NewPostButton'
 import NewPostForm from '@/components/NewPostForm'
 
-export default function CreatePostPage() {
+export default async function CreatePostPage() {
   return (
     <main className='flex w-screen h-screen justify-center'>
-      <NewPostButton />
+      <BackButton />
       <article className='flex flex-col m-5 w-[45rem] p-5 space-y-10'>
         <p className='text-5xl font-bold w-64 text-left self-center'>Criar nova postagem</p>
         <NewPostForm />
--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { BiLeftArrowAlt } from "react-icons/bi";
 
-export default function NewPostButton() {
+export default function BackButton() {
   return (
     <Link href='/home' className='flex items-center justify-center w-24 h-9 bg-neutral-800 ring-1 font-semibold ring-neutral-700 m-5 rounded absolute left-0 transition ease-in-out hover:bg-neutral-600'>
           <BiLeftArrowAlt size={20}/>
